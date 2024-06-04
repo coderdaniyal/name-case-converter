1b5632eaa325f8c3d36de95e5c8a837b719f4060
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-04 09:37:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var personName = "Daniyal";
+console.log("(lowercase) ".concat(personName.toLowerCase()));
+console.log("(UPPERCASE) ".concat(personName.toUpperCase()));
+console.log("(Titlecase) ".concat(personName));
---
+++ app.ts
@@ -0,0 +1,5 @@
+let personName:string = "Daniyal"
+
+console.log(`(lowercase) ${personName.toLowerCase()}`)
+console.log(`(UPPERCASE) ${personName.toUpperCase()}`)
+console.log(`(Titlecase) ${personName}`)
